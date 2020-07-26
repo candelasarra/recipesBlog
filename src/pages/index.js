d316fa1b2fbaaad635fcdc720a73f5e272bca3eb
Candela Sarrabayrouse <candela.sarra@gmail.com>
2020-07-26 16:42:21 -0400

    c

--- src/pages/index.js
+++ src/pages/index.js
@@ -163,6 +163,7 @@ const Index = props => {
       style={{
         backgroundImage:
           'url("https://cdn.inspirationhut.net/wp-content/uploads/2014/09/light-paper-fibers.jpg")',
+        transform: "translate3d(0,0,0)",
       }}
     >
       <MainWrapper>
