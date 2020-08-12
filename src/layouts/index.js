9c4d7d98ab391730f4e2c56d10ff49799d73e001
Candela Sarrabayrouse <candela.sarra@gmail.com>
2020-08-11 21:53:00 -0400

    document fix

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,6 +3,7 @@ import "./layout.css"
 import MainWrapper from "../templates/MainWrapper"
 import DarkLightThemeContext from "../templates/DarkLightTheme"
 import { makeStyles } from "@material-ui/core"
+import { getCookie } from "../commons/functions"
 const useStyles = makeStyles(() => ({
   darkTheme: {
     filter: "invert(1) hue-rotate(180deg)",
@@ -15,18 +16,6 @@ const useStyles = makeStyles(() => ({
 }))
 const Layout = ({ children, location }) => {
   const classes = useStyles()
-  function getCookie(name) {
-    if (document && document !== undefined) {
-      var nameEQ = name + "="
-      var ca = document.cookie.split(";")
-      for (var i = 0; i < ca.length; i++) {
-        var c = ca[i]
-        while (c.charAt(0) == " ") c = c.substring(1, c.length)
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length)
-      }
-      return null
-    }
-  }
   const [darkLightTheme, setDarkLightTheme] = useState(
     getCookie("darkTheme") ? getCookie("darkTheme") : "light"
   )
